2a748dc8fed5e1c98ab6bc0415e3b3bfbed6c656
Leonel <73618352+1LealSan1@users.noreply.github.com>
2022-11-29 14:25:18 -0700

    Update index.js

--- servidor/index.js
+++ servidor/index.js
@@ -8,9 +8,11 @@ const app = express();
 port=5000;
 
 //permite la conexion de expresss-angular
-app.use(cors({
-    origin: 'https://front-traders.vercel.app'
-}));
+app.use(cors())
+ 
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
 //permitimos el uso de json
 app.use(express.json());
@@ -21,6 +23,6 @@ conectarDB();
 app.use("/Users", require("./controllers/ControllerUserClient"));
 app.use("/UsersTraders", require("./controllers/ControllerUserTrader"));
 
-app.listen(port, cors(), () => {
-    console.log("el servidor corre en el puerto", port);
-})
+app.listen(5000, function () {
+    console.log('CORS-enabled web server listening on port',5000)
+  })
